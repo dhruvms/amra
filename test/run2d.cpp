ac71d0efd3e2367ecb106de9cef7fb9d956a52f0
Dhruv Mauria Saxena <dhruvmsaxena@gmail.com>
2021-06-07 17:47:44 -0400

    start, goal for cul-de-sac envs

--- test/run2d.cpp
+++ test/run2d.cpp
@@ -13,6 +13,8 @@ int main(int argc, char** argv)
 	std::string mapfile(argv[1]);
 	Environment env(mapfile);
 	env.CreateSearch();
+	env.SetStart(28, 20);
+	env.SetGoal(15, 45);
 	// env.CreateWAStarSearch(1.0);
 	env.Plan();
 }
