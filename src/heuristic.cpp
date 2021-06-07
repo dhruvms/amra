9c33a878788848672b1ddc3a46dc5d9f11790108
Dhruv Mauria Saxena <dhruvmsaxena@gmail.com>
2021-06-07 17:47:22 -0400

    variable renaming

--- src/heuristic.cpp
+++ src/heuristic.cpp
@@ -15,8 +15,8 @@ unsigned int EuclideanDist::GetGoalHeuristic(int state_id)
 	m_space->GetGoal(goal);
 	m_space->GetStateFromID(state_id, state);
 
-	double dist_sq = std::sqrt(std::pow(state.d1 - goal.d1, 2) + std::pow(state.d2 - goal.d2, 2));
-	return (dist_sq * COST_MULT);
+	double dist = std::sqrt(std::pow(state.d1 - goal.d1, 2) + std::pow(state.d2 - goal.d2, 2));
+	return (dist * COST_MULT);
 }
 
 unsigned int EuclideanDist::GetStartHeuristic(int state_id)
@@ -25,8 +25,8 @@ unsigned int EuclideanDist::GetStartHeuristic(int state_id)
 	m_space->GetStart(start);
 	m_space->GetStateFromID(state_id, state);
 
-	double dist_sq = std::sqrt(std::pow(state.d1 - start.d1, 2) + std::pow(state.d2 - start.d2, 2));
-	return (dist_sq * COST_MULT);
+	double dist = std::sqrt(std::pow(state.d1 - start.d1, 2) + std::pow(state.d2 - start.d2, 2));
+	return (dist * COST_MULT);
 }
 
 unsigned int EuclideanDist::GetFromToHeuristic(int from_id, int to_id)
@@ -35,8 +35,8 @@ unsigned int EuclideanDist::GetFromToHeuristic(int from_id, int to_id)
 	m_space->GetStateFromID(from_id, from);
 	m_space->GetStateFromID(to_id, to);
 
-	double dist_sq = std::sqrt(std::pow(from.d1 - to.d1, 2) + std::pow(from.d2 - to.d2, 2));
-	return (dist_sq * COST_MULT);
+	double dist = std::sqrt(std::pow(from.d1 - to.d1, 2) + std::pow(from.d2 - to.d2, 2));
+	return (dist * COST_MULT);
 }
 
 }  // namespace CMUPlanner
