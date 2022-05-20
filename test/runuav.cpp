6f6b87972712a5d287eafa8eec91e24b9555ea95
Dhruv Mauria Saxena <dhruvmsaxena@gmail.com>
2022-05-20 15:34:53 -0400

    run random start goal for uavs

--- test/runuav.cpp
+++ test/runuav.cpp
@@ -13,26 +13,26 @@ int main(int argc, char** argv)
     std::string mapfile(argv[1]);
     std::string mprimfile = "../dat/mprim/mhi_3m_9m.mprim";
 
-    // run UAV planning for fixed starts and goals
-    std::vector<std::vector<double> > starts, goals;
-    starts = { {981.000000, 639.000000, 3.665191, 0.000000} };
-    goals = { {297.000000, 180.000000, 3.665191, 0.000000} };
-    for (int i = 0; i < starts.size(); ++i)
-    {
-        GridUAV uav(mapfile);
-        uav.ReadMprims(mprimfile);
-        uav.CreateSearch();
-        uav.SetGoal(goals[i]);
-        uav.SetStart(starts[i]);
-        uav.Plan(true);
-    }
-
-    // // run UAV planning for random starts and goals
-    // for (int i = 0; i < 1; ++i)
+    // // run UAV planning for fixed starts and goals
+    // std::vector<std::vector<double> > starts, goals;
+    // starts = { {981.000000, 639.000000, 3.665191, 0.000000} };
+    // goals = { {297.000000, 180.000000, 3.665191, 0.000000} };
+    // for (int i = 0; i < starts.size(); ++i)
     // {
     //     GridUAV uav(mapfile);
     //     uav.ReadMprims(mprimfile);
     //     uav.CreateSearch();
+    //     uav.SetGoal(goals[i]);
+    //     uav.SetStart(starts[i]);
     //     uav.Plan(true);
     // }
+
+    // run UAV planning for random starts and goals
+    for (int i = 0; i < 1; ++i)
+    {
+        GridUAV uav(mapfile);
+        uav.ReadMprims(mprimfile);
+        uav.CreateSearch();
+        uav.Plan(true);
+    }
 }
